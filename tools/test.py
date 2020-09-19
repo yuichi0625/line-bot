0e61b301a36d0d0f0c43b97d4ba77b839034838d
yuichi0625 <yuichi.miyamae.rna@gmail.com>
2020-09-19 16:16:15 +0900

    test

--- tools/test.py
+++ tools/test.py
@@ -1,11 +1,12 @@
 import os
 import psycopg2
+from psycopg2.extras import DictCursor
 
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 with psycopg2.connect(DATABASE_URL) as conn:
-    with conn.cursor() as cur:
+    with conn.cursor(cursor_factory=DictCursor) as cur:
         cur.execute("select * from stations where pref_name = '東京都';")
         rows = cur.fetchall()
         print(rows)
