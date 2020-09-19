d21613450f341b79910f60ba6200bdccb9cc7102
yuichi0625 <yuichi.miyamae.rna@gmail.com>
2020-09-19 16:13:50 +0900

    test

--- tools/test.py
+++ tools/test.py
@@ -6,6 +6,6 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 with psycopg2.connect(DATABASE_URL) as conn:
     with conn.cursor() as cur:
-        cur.execute('SELECT * FROM stations')
+        cur.execute("select * from stations where pref_name = '東京都';")
         rows = cur.fetchall()
         print(rows)
