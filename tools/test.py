59bd6e65ff2806dddb49048551d6f42f19581453
yuichi0625 <yuichi.miyamae.rna@gmail.com>
2020-09-19 15:50:48 +0900

    postgresql

--- tools/test.py
+++ tools/test.py
@@ -1,4 +1,11 @@
 import os
-from pprint import pprint
+import psycopg2
 
-pprint(os.environ)
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+with psycopg2.connect(DATABASE_URL) as conn:
+    with conn.cursor() as cur:
+        cur.execute('SELECT * FROM stations WHERE pref_name = "東京都"')
+        rows = cur.fetchall()
+        print(rows)
